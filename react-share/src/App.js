1947041050fca90182077b843ebbfd60a7f1adc5
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-16 23:02:57 +0100

    Add styles to Media Card

--- react-share/src/App.js
+++ react-share/src/App.js
@@ -1,22 +1,10 @@
-import Signup from "./pages/Signup";
-import Navbar from "./components/Navbar";
-
+import AppRouter from "./router/Router";
+import AuthContextProvider from "./context/AuthContext";
 function App() {
-  console.log(process.env);
-
   return (
-    <div className="App">
-      <Navbar />
-      <Signup />
-      {/* 
-  
-  //Route
-  //Signin
-  //Signup
-  //forgotpassword
-  //....
-  */}
-    </div>
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
   );
 }
 
