85834ffe061636514a568ca7e563766c4e3278ce
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2020-12-20 00:29:31 +0100

    Add dateFormatter and CusromErrorHandler

--- react-share/src/helper/customErrorHandler.js
+++ react-share/src/helper/customErrorHandler.js
@@ -1,64 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import { customErrorHandler } from "../helper/customErrorHandler";
-//firestore
-
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-};
-
-const prodConfig = {};
-
-const config = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
-
-class Firebase {
-  constructor() {
-    //TODO: add initialize check :: if (firebase.apps.length === 0)
-    firebase.initializeApp(config);
-    this.firebaseAuth = firebase.auth();
-  }
-
-  // register registerWithEmailAndPassword
-  async register(displayName, email, password) {
-    try {
-      await this.firebaseAuth.createUserWithEmailAndPassword(email, password);
-      this.firebaseAuth.currentUser.updateProfile({
-        displayName,
-      });
-    } catch (err) {
-      console.log("F. Error:", err);
+export const customErrorHandler=(err=>{
+  
+    if(err.code==="auth/user-not-found"){
+        return "There is no user with this email" 
     }
-  }
-
-  // sign in/up with google GoogleAuthProvider
-  useGoogleProvider() {
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    googleProvider.setCustomParameters({ prompt: "select_account" });
-    this.firebaseAuth.signInWithPopup(googleProvider);
-  }
-
-  // login  signInWithEmailAndPassword
-  async signIn(email, password) {
-    try {
-     let res=await this.firebaseAuth.signInWithEmailAndPassword(email, password);
-      
-    } catch (error) {
-      return customErrorHandler(error)
+    if(err.code==="auth/email-already-in-use"){
+        return "The email address is already in use.Please try with another email"
     }
-  }
-
-  // logout signOut
-  signOut() {
-    this.firebaseAuth.signOut();
-  }
-
-  // forgot password sendPasswordResetEmail
-}
-
-export default new Firebase();
+    if(err.code==="auth/wrong-password"){
+        return "Wrong Password!! Please try again"
+    }
+ 
+    
+});
